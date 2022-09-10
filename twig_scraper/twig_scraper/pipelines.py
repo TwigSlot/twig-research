e71db572c018d0113e37aaa439e46d4f1ec96f8f
tch1001 <tanchienhao@gmail.com>
2022-09-10 17:52:30 +0800

    working neo4j pipeline

--- twig_scraper/twig_scraper/pipelines.py
+++ twig_scraper/twig_scraper/pipelines.py
@@ -6,8 +6,32 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import os
+from neo4j import GraphDatabase, Neo4jDriver
+from dotenv import load_dotenv
 
 class TwigScraperPipeline:
+    def __init__(self):
+        load_dotenv()
+        self.url = os.environ.get('NEO4J_SERVER_URL')
+        self.username = os.environ.get('NEO4J_USERNAME')
+        self.password = os.environ.get('NEO4J_PASSWORD')
+        print(f"connecting to neo4j {self.url}")
+        self.conn = GraphDatabase.driver(
+            self.url,
+            auth=(
+                self.username,
+                self.password
+            )
+        )
+        self.conn.verify_connectivity()
+        print("verified neo4j connection")
     def process_item(self, item, spider):
+        with self.conn.session() as session:
+            queryStr = f"MERGE (n:Url {{ url: $url }}) \
+                    -[e:Has_Title]->\
+                    (m:Title {{ title: $title }})"
+            session.run(queryStr, {'title': item['title'], 'url': item['url']})
         return item
+    def close_spider(self, spider):
+        self.conn.close()
--- twig_scraper/twig_scraper/spiders/wiki.py
+++ twig_scraper/twig_scraper/spiders/wiki.py
@@ -3,15 +3,22 @@ import scrapy
 class WikiSpider(scrapy.Spider):
     name = 'wiki'
     start_urls = ['https://en.wikipedia.org/wiki/Quantum_field_theory']
+    allowed_domains = ["en.wikipedia.org"]
 
     def __init__(self):
         self.state = {}
 
     def parse(self, response):
-        yield {
-            'title': response.css('span.mw-page-title-main::text').get(),
-            'url': response.url
-        }
+        title = response.css('span.mw-page-title-main::text').get()
+        if(title is None):
+            title = response.css('h1.firstHeading::text').get()
+        if(title):
+            yield {
+                'title': title,
+                'url': response.url
+            }
+        else:
+            return
         if('count' in self.state):
             self.state['count'] += 1
         else:
