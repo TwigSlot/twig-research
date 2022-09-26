924ed1b19ce3293f37b13115d40699d484aa7bf0
thewondefulcaterpillar <58139387+thewondefulcaterpillar@users.noreply.github.com>
2022-09-26 21:59:21 +0800

    Add files via upload
    no more html

---
+++ directpdf.py
@@ -0,0 +1,42 @@
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LTChar,LTLine,LAParams
+from collections import defaultdict
+d=defaultdict(int)
+PDF_file = open('pig.pdf', 'rb')
+
+for page_layout in extract_pages(PDF_file):
+    for element in page_layout:
+        if isinstance(element, LTTextContainer):
+            for text_line in element:
+                try:
+                    for character in text_line:
+                        if isinstance(character, LTChar):
+                            Font_size=character.size
+                            d[round(character.size)]+=1
+                except:
+                    continue
+mostcommonfontsize=-1
+localMax=0
+#this dict stores the value of the font and number of chars with font 
+for key in d.keys():
+    if d[key]>localMax:
+        localMax=d[key]
+        mostcommonfontsize=key
+sortedkeys=sorted([key for key in d.keys()])
+sortedkeys=sortedkeys[::-1]
+print(d)
+for page_layout in extract_pages(PDF_file):
+    for element in page_layout:
+        if isinstance(element, LTTextContainer):
+            for text_line in element:
+                try:
+                    for character in text_line:
+                        if isinstance(character, LTChar):
+                            Font_size=character.size
+                except:continue
+                text_line=text_line.get_text()
+                text_line=text_line.replace(u'\n', u' ')
+                if round(Font_size)>mostcommonfontsize:
+                    print("=== "*(sortedkeys.index(round(Font_size))),text_line)
+
+
