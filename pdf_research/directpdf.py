bc4b438561e3ebd59e815393187b03a2e23d0fed
tch1001 <tanchienhao@gmail.com>
2022-09-27 15:08:26 +0800

    todo: use table of contents

--- pdf_research/directpdf.py
+++ pdf_research/directpdf.py
@@ -1,9 +1,42 @@
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar,LTLine,LAParams
 from collections import defaultdict
+
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+
+
 d=defaultdict(int)
 fonts = defaultdict(str)
-PDF_file = open('./pdf_research/test_pdfs/example.pdf', 'rb')
+path = "pdf_research/test_pdfs/371d526793649d12487cba5350aeda7888422af1428933d094ca8cbfb64209c3.pdf"
+PDF_file = open(path, 'rb')
+
+parser = PDFParser(PDF_file)
+document = PDFDocument(parser)
+try:
+    print(list(document.get_outlines())) # TODO for caterpillar
+    """
+    for your ref,
+    https://stackoverflow.com/questions/51436686/extract-text-from-pdf-table-of-contents-ignoring-page-and-indexing-numbers
+    it is a list of tuples of (level, title, dest, a (href link like in html), se)
+    use the level and title info 
+    use a href info to get the content of that section
+    to create a json "TREE" of the document
+    i.e.
+    everything_in_json = {
+        'Short-term ...' : {
+            'Abstract': {
+                'content of abstract here'
+            },
+            'Introduction': {
+                'introduction body here'
+            }
+        }
+    }
+    """
+    exit()
+except:
+    print("no table of contents zzz, we are gonna have to analyse font size... ")
 
 for page_layout in extract_pages(PDF_file):
     for element in page_layout:
@@ -20,7 +53,8 @@ for page_layout in extract_pages(PDF_file):
                     continue
                 for ((size, font), text) in line_fonts.items():
                     d[size] += len(text)
-                    fonts[(size,font)] += text + "\n"
+                    fonts[(size,font)] += text + " "
+
 mostcommonfontsize=-1
 localMax=0
 #this dict stores the value of the font and number of chars with font 
