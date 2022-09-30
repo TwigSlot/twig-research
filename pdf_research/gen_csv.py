977881fa0b2ff6696bc74721e1e323c8dedd7599
tch1001 <tanchienhao@gmail.com>
2022-09-30 12:50:51 +0800

    oops left out the data

--- pdf_research/gen_csv.py
+++ pdf_research/gen_csv.py
@@ -6,16 +6,18 @@ import csv
 from extract_textblock import TextBlock
 
 if 'pdf_research' in os.getcwd():
-    path = './Classifiers/'
+    write_path = './Classifiers/'
+    read_path = './test_pdfs/'
 else:
-    path = 'pdf_research/Classifiers/'
+    write_path = 'pdf_research/Classifiers/'
+    read_path = './pdf_research/test_pdfs/'
 
-with open(path+'data.csv', 'w') as csv_file:
+with open(write_path+'data.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(TextBlock.headers())
-    for i in os.listdir(path):
+    for i in os.listdir(read_path):
         if(i[-7:] == '.pickle'):
-            with open(path+i, 'rb') as f:
+            with open(read_path+i, 'rb') as f:
                 doc = pickle.load(f)
                 for tb in doc:
                     writer.writerow(tb.properties())
